c0cf2617044c26b3fc72c3a15f9b7f1206f6e9b1
mahi-19 <upadhyayvivek.19@gmail.com>
2023-05-21 09:58:53 +0530

    Added animanted icons

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,9 @@
-import ai from "./ai.png";
-import fileIcon from "./file.png";
-import swatch from "./swatch.png";
-import download from "./download.png";
+import ai from "./translator.gif";
+import fileIcon from "./edit.gif";
+import swatch from "./color.gif";
+import download from "./download.gif";
 
-import logoShirt from "./logo-tshirt.png";
+import logoShirt from "./tshirt.gif";
 import stylishShirt from "./stylish-tshirt.png";
 
 export { ai, fileIcon, swatch, download, logoShirt, stylishShirt };
