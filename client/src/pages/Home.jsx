6e880c7ff522ae8a34ba8ac545447d744aababf4
AkshatChaube01 <AkshatChaube11@gmail.com>
2023-05-22 02:56:41 +0530

    Dark-Theme

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,24 +10,31 @@ import {
     slideAnimation
 } from '../config/motion'
 
-const Home = () => {
+const Home = ({props}) => {
     const snap = useSnapshot(state);
-    return (
+
+    return (   
         <AnimatePresence>
             {snap.intro && (
                 <motion.section className="home" {...slideAnimation('left')}>
                     <motion.header {...slideAnimation("down")}>
+                        <div className="up">
                         <img
                          src="./threejs.png"
                          alt="logo"
-                         className="w-8 h-8 object-contain" />
+                         className="w-10 h-10 object-contain" />
+                        </div>
                     </motion.header>
 
                     <motion.div className="home-content" {...headContainerAnimation}>
                         <motion.div {...headTextAnimation}>
-                        <h1 className="head-text">
+                        
+                            <div className={props && props.isActive ? 'head-text' : 'head-text-dark'}>
+                            <h1 >
                             LET'S <br className="xl:block hidden" /> DO IT.
-                        </h1>
+                            </h1>
+                            </div>
+                        
                         </motion.div>
                         <motion.div
                             {...headContentAnimation}
@@ -36,21 +43,23 @@ const Home = () => {
                             <p>
                                 Create your unique and exlusive shirt with our
                                 brand-new 3D customization tool.
-                                <strong>Unleash your imagination</strong>
+                                <strong> Unleash your imagination</strong>
                                 {" "} and define your own style.
                             </p>
 
                             <CustomButton
                                 type="filled"
-                                title="Customize It"
+                                title="Customize it"
                                 handleClick={() => state.intro = false}
                                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
                             />
+
                         </motion.div>
                     </motion.div>
 
                 </motion.section>
             )}
+        
         </AnimatePresence>
     )
 }
