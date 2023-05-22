7370d00fec50488f92233da74fc9b139fb90b85c
mahi-19 <upadhyayvivek.19@gmail.com>
2023-05-22 23:13:35 +0530

    Added Changes

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,4 @@
-import { swatch, fileIcon, ai, logoShirt, stylishShirt } from "../assets";
+import { swatch, fileIcon, ai, logoShirt, stylishShirt ,download} from "../assets";
 
 export const EditorTabs = [
   {
@@ -12,6 +12,11 @@ export const EditorTabs = [
   {
     name: "aipicker",
     icon: ai,
+  },
+  {
+    name: "download",
+    icon: download,
+
   },
 ];
 
