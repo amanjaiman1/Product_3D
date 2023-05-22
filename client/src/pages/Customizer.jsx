2e387e7d5ac32b735b776387d7b3b3bae46cec84
mahi-19 <upadhyayvivek.19@gmail.com>
2023-05-22 22:57:35 +0530

    Fix The Download button

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -213,6 +213,13 @@ const Customizer = () => {
                 handleClick={() => handleActiveFilterTab(tab.name)}
               />
             ))}
+               <button className='download-btn' onClick={downloadCanvasToImage}>
+              <img
+                src={download}
+                alt='download_image'
+                className='w-3/5 h-3/5 object-contain'
+              />
+            </button>
           </motion.div>
         </>
       )}
