93b242aced2301acd9436eae1bf7440918bb2aa7
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-05-23 10:26:03 +0530

    fix : user model

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     googleId:{type:String},
+    email:{type:String,required:true},
     firstName:{type:String,required:true},
     lastName:{type:String},
     profile:{type:String},
