f01c3f73e38c8e829f54c320686d684d83391ad6
Harsh Patil <harshpatil2956@gmail.com>
2023-05-24 22:39:19 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
-import Canvas from "./canvas"
-import Customizer from "./pages/Customizer"
-import Home from "./pages/Home"
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
+import ErrorPage from "./components/404Error";
 import React, { useState } from "react";
-import { CustomButton } from './components'
+import { CustomButton } from "./components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [active, setActive] = useState(false);
@@ -10,20 +12,38 @@ function App() {
     setActive(!active);
   };
   return (
-    <main className="app transition-all ease-in " style={{ backgroundColor: active ? "black" : "white",color :active ?"white":"black" }}>
-      <div className={'new'} >
-             <CustomButton
-              type="filled"
-              title="Theme"
-              handleClick={handleClick}
-              />
+    <main
+      className="app transition-all ease-in "
+      style={{
+        backgroundColor: active ? "black" : "white",
+        color: active ? "white" : "black",
+      }}
+    >
+      <div className={"new"}>
+        <CustomButton type="filled" title="Theme" handleClick={handleClick} />
       </div>
-      <Home isActive={active} />
-      <Canvas />
-      <Customizer />
+      <div className={"new"}>
+        <Customizer type="filled" handleClick={handleClick} />
+      </div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Home isActive={active} />
+                <Canvas />
+              </>
+            }
+          />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
+
 
