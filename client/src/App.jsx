76297224b016645c9611dba21191f1699177bc47
Harsh Patil <harshpatil2956@gmail.com>
2023-05-24 21:15:45 +0530

    App.jsx
    Routing added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import Canvas from "./canvas"
 import Customizer from "./pages/Customizer"
 import Home from "./pages/Home"
+import ErrorPage from "./components/404Error"
 import React, { useState } from "react";
 import { CustomButton } from './components'
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+
 
 function App() {
   const [active, setActive] = useState(false);
@@ -12,18 +15,32 @@ function App() {
   return (
     <main className="app transition-all ease-in " style={{ backgroundColor: active ? "black" : "white",color :active ?"white":"black" }}>
       <div className={'new'} >
-             <CustomButton
+             <CustomButton 
               type="filled"
               title="Theme"
               handleClick={handleClick}
               />
       </div>
-      <Home isActive={active} />
-      <Canvas />
+      <BrowserRouter>
+      <Routes>
+      <Route path = "/" element = {<Home isActive={active}></Home> }/>
+      <Route path ="/" element = {<Canvas />}/>
+      <Route path ="*" element = {<ErrorPage />}/>
+
+      </Routes>
+      </BrowserRouter>
       <Customizer />
+    
     </main>
   )
 }
 
 export default App
+
+
+
+
+
+
+
 
