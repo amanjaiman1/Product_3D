f3b0985b6314283577041cfe527a2edec9636224
Sarthak Bathla <sarthakbathla17@gmail.com>
2023-05-27 12:51:13 +0530

    Just fixed my mistake

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 require('dotenv').config()
 
-app.listen(process.env.PORT || 5000, () =>{
+app.listen(process.env.PORT, () =>{
     console.log(`Server Connected On ${process.env.PORT}`)}
 )
