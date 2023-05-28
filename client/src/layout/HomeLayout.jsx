8c816cd4735f8e4c3d42d5ce7deafc9b263ec0bb
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-05-28 16:33:10 +0530

    ui : removed padding for responsive

--- client/src/layout/HomeLayout.jsx
+++ client/src/layout/HomeLayout.jsx
@@ -6,8 +6,8 @@ function HomeLayout({ children }) {
   return (
     <div className="bg-gradient-to-t from-slate-50 to-blue-100">
       <Header />
-      <main className="px-10">
-        <section class="container mx-auto px-4 py-8">{children}</section>
+      <main className="min-[400px]:px-10">
+        <section className="container mx-auto px-4 py-8">{children}</section>
       </main>
       <Footer />
     </div>
