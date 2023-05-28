bdda8768c8d4421cebefdbe43b20f9b7f42e51c9
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-05-28 18:09:50 +0530

    chore: refactor team section

---
+++ client/src/components/ContributorCard/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function ContributorCard({contributor,index}) {
+  return (
+    <div
+      key={contributor?.id}
+      className={`flex flex-col items-center space-y-4 text-center  ${
+        index % 10 >= 8 ? "ml-8" : ""
+      }`}
+    >
+      <a
+        rel="noopener noreferrer"
+        href={`https://github.com/${contributor?.login}`}
+        aria-label="GitHub"
+        className="p-2 rounded-md hover:text-violet-400"
+      >
+        <img
+          src={contributor?.avatar_url}
+          alt=""
+          className="w-32 h-32 mx-auto rounded-full"
+        />
+      </a>
+      <div className="space-y-1">
+        <h2 className="text-xl font-semibold text-violet">
+          {contributor?.login}
+        </h2>
+        <p className="text-xs">{`Contributions: ${contributor?.contributions}`}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ContributorCard;
---
+++ client/src/components/Pagination/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Pagination({ data, perPage, currentPage, paginate }) {
+  return (
+    <>
+      <ul className={`flex justify-center space-x-4 `}>
+        {Array.from(
+          { length: Math.ceil(data.length / perPage) },
+          (_, index) => (
+            <li
+              key={index}
+              className={`cursor-pointer ${
+                currentPage === index + 1 ? "font-bold" : ""
+              }`}
+              onClick={() => paginate(index + 1)}
+            >
+              {index + 1}
+            </li>
+          )
+        )}
+      </ul>
+    </>
+  );
+}
+
+export default Pagination;
