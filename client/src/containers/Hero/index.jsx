37b71549d6f9763dc0dce7c99159c2d3f28fcb4d
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-05-28 16:32:39 +0530

    ui: responsive for hero section

--- client/src/containers/Hero/index.jsx
+++ client/src/containers/Hero/index.jsx
@@ -4,25 +4,25 @@ import Button from "../../components/Button";
 
 function Hero() {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex justify-center items-center flex-wrap-reverse">
       <div className="flex-1">
-        <span style={{backgroundColor:"#F8FAFF"}} className="text-primary text-opacity-90 font-semibold p-2 rounded">
+        <span className="text-primary bg-hero-badge-bg text-opacity-90 font-semibold p-2 rounded max-[600px]:flex max-[600px]:justify-center">
           AI-Powered 3D T-Shirts
         </span>
         <div className="py-3">
-          <h1 className="text-heading font-semibold text-5xl leading-normal">
+          <h1 className="text-heading font-semibold min-[600px]:text-5xl max-[600px]:text-4xl min-[600px]:leading-normal max-[600px]:leading-tight max-[600px]:text-center">
             Elevate Your Style with Intelligent Fashion
           </h1>
-          <p className="text-secondry font-normal pt-3 leading-loose">
-            Indulge in the mesmerizing world of 3D fashion and <br /> elevate your
-            style with our extraordinary collection of <br /> vibrant designs.
+          <p className="text-secondry font-normal pt-3 leading-loose max-[600px]:text-center">
+            Indulge in the mesmerizing world of 3D fashion and elevate your
+            style with our extraordinary collection of vibrant designs.
           </p>
         </div>
-        <div className="pt-3">
-          <Button >Design Now</Button>
+        <div className="pt-3 max-[600px]:flex max-[600px]:justify-center">
+          <Button>Design Now</Button>
         </div>
       </div>
-      <div className="flex-2">
+      <div className="flex-3">
         <img src={hero} alt="" />
       </div>
     </div>
