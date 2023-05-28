7a493fff5bbf86bd541e6a03873dbd845ce3fbab
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-05-28 15:55:52 +0530

    ui: added padding in main section and also update background for hero section

--- client/src/layout/HomeLayout.jsx
+++ client/src/layout/HomeLayout.jsx
@@ -4,13 +4,13 @@ import Footer from "../containers/Footer";
 
 function HomeLayout({ children }) {
   return (
-    <>
+    <div className="bg-gradient-to-t from-slate-50 to-blue-100">
       <Header />
-      <main>
+      <main className="px-10">
         <section class="container mx-auto px-4 py-8">{children}</section>
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
