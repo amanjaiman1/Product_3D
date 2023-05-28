f9a2ef7a45de273a6e7a3a2cff3a2cee4bb3f874
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-05-28 15:58:02 +0530

    feat: added button component

---
+++ client/src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button({children,rest}) {
+  return (
+    <button className='bg-primary hover:bg-blue-700 outline-none p-3 rounded-lg text-white' {...rest}>{children}</button>
+  )
+}
+
+export default Button
