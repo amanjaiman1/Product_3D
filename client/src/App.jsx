ee5be3e7084a50e53a12763237ee8aed03be5a03
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-05-28 14:20:40 +0530

    added index.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/app/Home";
 import ErrorPage from "./views/app/Error";
+import './index.css'
 function App() {
   return (
     <BrowserRouter>
