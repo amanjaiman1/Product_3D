9337ff258289dd903c03f7f49984d3417184da58
mahi-19 <upadhyayvivek.19@gmail.com>
2023-06-01 00:42:25 +0530

    Added Highlights Section

--- client/src/containers/Footer/index.jsx
+++ client/src/containers/Footer/index.jsx
@@ -5,21 +5,21 @@ import { footerLinks } from ".";
 
 function Footer() {
   return (
-    <footer className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+    <footer className={`${styles.flexCenter} ${styles.paddingY} flex-col `}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full `}>
       <div className="flex-1 flex flex-col justify-start mr-10">
         <img src={objectFoot} alt="3D-Tee" className="w-[500px] h-[265px] object-contain" />
       </div>
 
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10 mr-10">
         {footerLinks.map((footerLink) => (
-          <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
+          <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px] ">
 
             <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-[#090F4E]">
               {footerLink.title}
             </h4>
 
-            <ul className="list-none mt-4">
+            <ul className="list-none mt-4 cursor-pointer">
               {footerLink.links.map((link, index) => (
                 <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-[#63657E] hover:text-[#6058f2] cusror-pointer ${styles.index !== footerLink.links.length -1 ? 'mb-4' : 'mb-0'}`}>
                   {link.name}
