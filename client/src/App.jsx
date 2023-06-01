acfcbf918f4709053a86611e10065bc7ecad8c31
Manisha Swain <manishaswain2003@gmail.com>
2023-06-01 20:50:49 +0530

    Frontend:added ScrollToTop 138

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Login from "./views/auth/Login";
 import SignUp from "./views/auth/SignUp";
 import blog from "./pages/Faq/Blog";
 import { favicon } from "./assets";
-
+import ScrollToTop from "react-scroll-to-top";
 import MoonLoader from "react-spinners/MoonLoader";
 function App() {
   const [loading, setLoading] = useState(false);
@@ -42,6 +42,16 @@ function App() {
             <Route Component={SignUp} path="/signup" />
             <Route Component={blog} path="/blog" />
           </Routes>
+
+          <div>
+            <ScrollToTop
+              smooth
+              className="scrlltop"
+              viewBox="-60 5 270 160"
+              top="100"
+              color="white"
+            />
+          </div>
         </BrowserRouter>
       )}
     </div>
