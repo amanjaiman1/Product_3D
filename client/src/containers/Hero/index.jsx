e4fd4c3df60459431bdcd4d13808f6c814fa56c6
PARAS <mehtaparas1901@gmail.com>
2023-06-01 20:40:35 +0530

    modify index.html

--- client/src/containers/Hero/index.jsx
+++ client/src/containers/Hero/index.jsx
@@ -9,7 +9,7 @@ function Hero() {
         <span className="text-primary bg-hero-badge-bg bg-opacity-60 text-opacity-70 font-semibold p-2 rounded max-[600px]:flex max-[600px]:justify-center">
           AI-Powered 3D T-Shirts
         </span>
-        <div className="py-3">
+        <div className="py-3" data-aos="flip-left">
           <h1 className="text-heading font-semibold min-[600px]:text-5xl max-[600px]:text-4xl min-[600px]:leading-normal max-[600px]:leading-tight max-[600px]:text-center">
             Elevate Your Style with Intelligent Fashion
           </h1>
@@ -22,7 +22,7 @@ function Hero() {
           <Button>Design Now</Button>
         </div>
       </div>
-      <div className="flex-3">
+      <div className="flex-3 ">
         <img src={hero} alt="" />
       </div>
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -15,5 +15,6 @@ export default {
       }
     },
   },
+
   plugins: [require('tailwind-scrollbar')],
 };
