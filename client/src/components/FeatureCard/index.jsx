57fb113d36a6b6b3513fe73a45bf233854cf11f8
abhi03ruchi <105661636+abhi03ruchi@users.noreply.github.com>
2023-06-08 09:32:20 +0530

    Resolved Flickering Effect

--- client/src/components/FeatureCard/index.jsx
+++ client/src/components/FeatureCard/index.jsx
@@ -1,33 +1,41 @@
 import React, { useState } from 'react';
 import './style.css';
-function FeatureCard({ title, color, icon, desc }) {
 
-  const [flip, setFlip] = useState(false)
+function FeatureCard({ title, color, icon, desc }) {
+  const [flip, setFlip] = useState(false);
+
   const handleCardHover = () => {
-    setFlip(true);
+    setTimeout(() => {
+      setFlip(true);
+    }, 100);
   };
 
   const handleCardLeave = () => {
-    setFlip(false);
+    setTimeout(() => {
+      setFlip(false);
+    }, 100);
   };
 
   return (
-    <div className='card-grid' >
-      <div  onMouseEnter={handleCardHover}
-        onMouseLeave={handleCardLeave} className={`card ${flip ? 'flip' : ''}`} >
+    <div className='card-grid'>
+      <div
+        onMouseEnter={handleCardHover}
+        onMouseLeave={handleCardLeave}
+        className={`card ${flip ? 'flip' : ''}`}
+      >
         {/* front */}
-        <div className="front ">
-          <div className="flex justify-center items-center rounded-lg w-16 h-16 mx-auto " style={{ backgroundColor: color }}>
+        <div className="front">
+          <div className="flex justify-center items-center rounded-lg w-16 h-16 mx-auto" style={{ backgroundColor: color }}>
             <img src={icon} alt="" />
           </div>
-          <div className=" dark-primary font-Poppins my-6 text-2xl font-semibold text-center mx-auto px-4">{title}</div>
-          <div></div>
+          <div className="dark-primary font-Poppins my-6 text-2xl font-semibold text-center mx-auto px-4">
+            {title}
+          </div>
         </div>
         {/* back */}
-        <div className="back"> {desc}  </div>
+        <div className="back">{desc}</div>
       </div>
     </div>
-
   );
 }
 
