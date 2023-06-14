c8cea069b1a612a1f5a876dad33b16365c1862f2
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-06-14 10:18:19 +0530

    feat: add customizer tab

--- client/src/components/CustomizerTabs/index.jsx
+++ client/src/components/CustomizerTabs/index.jsx
@@ -1,38 +1,46 @@
-import React from "react";
+import React,{useState} from "react";
+import CustomizerTabPanel from "../CustomizerTabPanel";
+import CustomizerDesign from "../CustomizerDesign";
+import CustomizerColor from "../CustomizerColor";
+import CustomizerText from "../CustomizerText";
+import CustomizerLogo from "../CustomizerLogo";
 
 function CustomizerTabs() {
+  const [currentIndex, setcurrentIndex] = useState(0)
   const data = [
     {
       tabId: 1,
       tabName: "Design",
-      tabPanel: <div></div>,
+      tabPanel: <CustomizerDesign />
+
     },
     {
       tabId: 2,
       tabName: "Colors",
-      tabPanel: <div></div>,
+      tabPanel: <CustomizerColor />
+
     },
     {
       tabId: 3,
       tabName: "Text",
-      tabPanel: <div></div>,
+      tabPanel: <CustomizerText />,
     },
     {
       tabId: 3,
       tabName: "Logos",
-      tabPanel: <div></div>,
+      tabPanel: <CustomizerLogo />,
     },
   ];
   return (
     <div>
-      <div className="flex justify-between px-16">
-        {data.map((e) => (
-          <ul className="m-5">{e.tabName}</ul>
-        ))}
+      <div className="flex justify-center items-center mt-10">
+        {data && data?.map((item,index)=> {
+          return <ul onClick={()=>setcurrentIndex(index)} className={` cursor-pointer font-semibold text-black relative px-5 ${index==currentIndex&&"text-primary before:block before:absolute before:h-[2px] before:w-[25px] before:rounded-full before:top-[25px] before:bg-primary relative inline-block"}`}>{item?.tabName}</ul>
+        })}
       </div>
-      <div className="flex">
-        
-      </div>
+      <div className="flex justify-center items-center mt-10  w-full ">
+        <CustomizerTabPanel currentIndex={currentIndex} data={data}/>
+        </div>
     </div>
   );
 }
