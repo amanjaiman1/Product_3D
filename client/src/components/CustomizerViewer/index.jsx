80551c107abdf7f6b4577824abbf3dbfa10a72e6
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-06-14 10:17:21 +0530

    feat: customizer setup

--- client/src/components/CustomizerViewer/index.jsx
+++ client/src/components/CustomizerViewer/index.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function CustomizerViewer() {
   return (
-    <div className='bg-gray-500 w-[500px] h-[500px] absolute left-28'>
-      
+    <div className='w-[500px] h-[500px] absolute left-28'>
+        
     </div>
   )
 }
