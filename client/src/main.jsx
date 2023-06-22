d4119978ad31551d2217915b6d9ca46738ea90d3
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-06-22 13:21:42 +0530

    chore(husky): tested husky prettier

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./store/index";
+import store from "./store/index"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+<App />
     </Provider>
   </React.StrictMode>
 );
