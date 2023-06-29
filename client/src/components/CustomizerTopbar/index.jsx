3f2d63337c41d414d2a4e953beb6fda5d29362e9
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-06-29 16:08:07 +0530

    feat: added customizer topbar for customizer home page

---
+++ client/src/components/CustomizerTopbar/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "../Button";
+import { useNavigate } from "react-router-dom";
+
+function CustomizerTopbar() {
+  const navigate = useNavigate();
+  return (
+    <div className="shadow m-3 p-3 flex justify-between items-center">
+      <div>Recents</div>
+      <div className="flex space-x-2">
+        <Button
+          onClick={() => {
+            navigate("/app/customizer/editor");
+          }}
+        >
+          Design New T-shirt
+        </Button>
+        <Button className={"bg-pink-600 hover:bg-pink-500"}>Import</Button>
+      </div>
+    </div>
+  );
+}
+
+export default CustomizerTopbar;
