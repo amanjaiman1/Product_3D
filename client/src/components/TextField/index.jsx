9572eedf7bca013bb5f583b85117c03a217a4e4d
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-07-03 23:43:19 +0530

    style: update  text field

--- client/src/components/TextField/index.jsx
+++ client/src/components/TextField/index.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import { cn } from "../../utils/utils";
 
 function TextField({ className, ...rest }) {
-  return <input className={cn("", className)} {...rest} />;
+  return (
+    <input
+      className={cn(
+        "p-2 border-2 border-gray-200 border-solid rounded-[3px] outline-none",
+        className
+      )}
+      {...rest}
+    />
+  );
 }
 
 export default TextField;
