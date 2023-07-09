dca8f444b1c562aa8602066d23da8d60e2a8d021
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-07-09 13:59:35 +0530

    Revert "Cards alignment  and sidebar of customizer fixed"

--- client/src/components/CustomizerSidebar/index.jsx
+++ client/src/components/CustomizerSidebar/index.jsx
@@ -6,13 +6,13 @@ function CustomizerSidebar() {
   const selectedvalue = "bg-blue-700";
   const navigate = useNavigate();
   return (
-    <div className="p-4 shadow-lg bg-primary min-[320px]:w-[112px] sm:w-[100%]">
+    <div className="p-4 shadow-lg bg-primary">
       <div
         onClick={() => {
           setisActive((prev) => !prev);
           navigate("/app/customizer");
         }}
-        className={`cursor-pointer  mt-4 p-3 text-white text-center ${
+        className={`cursor-pointer  mt-4 p-3 text-white  ${
           isActive ? "" : selectedvalue
         }`}
       >
@@ -20,7 +20,7 @@ function CustomizerSidebar() {
       </div>
       <div
         onClick={() => setisActive((prev) => !prev)}
-        className={`cursor-pointer  mt-4 p-3 text-white text-center ${
+        className={`cursor-pointer  mt-4 p-3 text-white  ${
           isActive ? selectedvalue : "hover:bg-blue-700"
         }`}
       >
