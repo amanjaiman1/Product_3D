eab43171afc34737fddbe05ad23f5546fce9ded6
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2023-07-15 21:59:49 +0530

    feat: make testimonials accessible

--- client/src/containers/TeamPost/TeamPostCard.jsx
+++ client/src/containers/TeamPost/TeamPostCard.jsx
@@ -29,17 +29,17 @@ const Testimonials = () => {
           {cardData.map((card, index) => (
             <SwiperSlide key={index}>
               <div className="te bg-[#F8F9FF] h-60 md:h-48 w-[90%] md:w-[50%] p-7 rounded-[1.5rem]">
-                <div className="flex flex-row gap-x-4 pt-4 justify-center items-center">
+                <figure className="flex flex-row gap-x-4 pt-4 justify-center items-center">
                   <div>
                     <img src={img} alt={card.name} className="max-w-[60px]" />
                   </div>
-                  <div className="flex items-start flex-col">
+                  <figcaption className="flex items-start flex-col">
                     <h1 className="font-semibold text-xl h-full">{card.name}</h1>
                     <p className="font-normal">{card.role}</p>
-                  </div>
-                </div>
+                  </figcaption>
+                </figure>
                 <div className="mt-6 items-center">
-                  <p className="text-slate-500">{card.testimonial}</p>
+                  <blockquote className="text-slate-500">{card.testimonial}</blockquote>
                 </div>
               </div>
             </SwiperSlide>
