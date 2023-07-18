8afb7f24581d553e3647d86577a29d8a731b73bd
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-07-18 23:28:31 +0530

    fix: update profile container

--- src/containers/ProfileContainer/index.jsx
+++ src/containers/ProfileContainer/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "./../../components/TextField";
 import FormGroup from "../../components/FormGroup";
 import Avatar from "../../components/Avatar";
@@ -8,27 +8,37 @@ import PopupModel from "../../components/PopupModel";
 import ProfileEditor from "../ProfileEditor";
 function ProfileContainer() {
   const [isVisible, setIsVisible] = useState(false);
-  const userInfo = useSelector((state) => state.userInfo);
-  return (
+  const [userInfo, setuserInfo] = useState({
+    loading: true,
+    data: {},
+  });
+  useEffect(() => {
+    async function getUserInfo() {
+      let localUser = await JSON.parse(localStorage.getItem("userInfo"));
+      setuserInfo((prev) => {
+        return { data: localUser, loading: false };
+      });
+    }
+    getUserInfo();
+  }, []);
+  // console.log(user);
+
+  return userInfo.loading ? (
+    <h1>Loading..</h1>
+  ) : (
     <div className="flex flex-col p-10">
       <div className="flex justify-center items-center">
-        <Avatar user={userInfo} className={"w-28 h-28"} />
+        <Avatar user={userInfo?.data} className={"w-28 h-28"} />
       </div>
       <div className="flex flex-col space-y-3 mt-5">
         <FormGroup title={"Name"}>
-          <h1 className="text-secondary">xxxx</h1>
-        </FormGroup>
-        <FormGroup title={"User Name"}>
-          <h1 className="text-secondary">@xxxx</h1>
+          <h1 className="text-secondary">{userInfo?.data?.fullName}</h1>
         </FormGroup>
         <FormGroup title={"Job"}>
-          <h1 className="text-secondary">Designer</h1>
+          <h1 className="text-secondary">Desinger</h1>
         </FormGroup>
         <FormGroup title={"Email"}>
-          <h1 className="text-secondary">example@gmail.com</h1>
-        </FormGroup>
-        <FormGroup title={"Mobile"}>
-          <h1 className="text-secondary">+91-15262833930</h1>
+          <h1 className="text-secondary">{userInfo?.data?.email}</h1>
         </FormGroup>
       </div>
       <div className="mt-5">
