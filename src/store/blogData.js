6f98da2dd0ddefe40946afdd768405684e2f40d6
kaur-rajdeep <kaurd3535@gmail.com>
2023-07-25 21:04:27 +0530

    style: changed images path

--- src/store/blogData.js
+++ src/store/blogData.js
@@ -1,37 +1,37 @@
 export default [
   {
     id: 1,
-    image: "./src/assets/image/tips.webp",
+    image: "/tips.webp",
     title:
       "From Concept to Creation: Proven Tips for Crafting Striking T-shirt Designs",
   },
 
   {
     id: 2,
-    image: "./src/assets/image/design.webp",
+    image: "/design.webp",
     title: "A Guide to T-shirt Design Success: Tips and Techniques You Need to Know",
   },
   {
     id: 3,
-    image: "./src/assets/image/pro.webp",
+    image: "/pro.webp",
     title:
       "Design Like a Pro: 10 Essential Tips for Creating Professional T-shirt Artwork",
   },
   {
     id: 4,
-    image: "./src/assets/image/tips.webp",
+    image: "/tips.webp",
     title:
       "From Concept to Creation: Proven Tips for Crafting Striking T-shirt Designs",
   },
 
   {
     id: 5,
-    image: "./src/assets/image/design.webp",
+    image: "/design.webp",
     title: "A Guide to T-shirt Design Success: Tips and Techniques You Need to Know",
   },
   {
     id: 6,
-    image: "./src/assets/image/pro.webp",
+    image: "/pro.webp",
     title:
       "Design Like a Pro: 10 Essential Tips for Creating Professional T-shirt Artwork",
   },
