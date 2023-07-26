2ea46a77e80feefe235408647fa1c0139c6db0ea
kaur-rajdeep <kaurd3535@gmail.com>
2023-07-26 10:18:25 +0530

    style: cleaned up the code and fixed some styles

--- src/store/blogData.js
+++ src/store/blogData.js
@@ -1,37 +1,41 @@
+import tips from "./../assets/image/tips.webp";
+import design from "./../assets/image/design.webp";
+import pro from "./../assets/image/pro.webp";
+
 export default [
   {
     id: 1,
-    image: "/tips.webp",
+    image: tips,
     title:
       "From Concept to Creation: Proven Tips for Crafting Striking T-shirt Designs",
   },
 
   {
     id: 2,
-    image: "/design.webp",
+    image: design,
     title: "A Guide to T-shirt Design Success: Tips and Techniques You Need to Know",
   },
   {
     id: 3,
-    image: "/pro.webp",
+    image: pro,
     title:
       "Design Like a Pro: 10 Essential Tips for Creating Professional T-shirt Artwork",
   },
   {
     id: 4,
-    image: "/tips.webp",
+    image: tips,
     title:
       "From Concept to Creation: Proven Tips for Crafting Striking T-shirt Designs",
   },
 
   {
     id: 5,
-    image: "/design.webp",
+    image: design,
     title: "A Guide to T-shirt Design Success: Tips and Techniques You Need to Know",
   },
   {
     id: 6,
-    image: "/pro.webp",
+    image: pro,
     title:
       "Design Like a Pro: 10 Essential Tips for Creating Professional T-shirt Artwork",
   },
