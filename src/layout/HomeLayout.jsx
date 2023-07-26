1b2308e1504756f5b7ed57b904479ba5d31da3f1
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-07-26 14:51:53 +0530

    feat: added the development branch link

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 function HomeLayout({ children }) {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-screen bg-black text-white">
       <h1 className={"text-3xl"}>The Site is under maintenance</h1>
       <Button onClick={() => navigate("/login")} className={"mt-5"}>
         Go to Design{" "}
@@ -21,6 +21,20 @@ function HomeLayout({ children }) {
       >
         Contact Team{" "}
       </Link>
+
+      <h2 className="mt-10 text-[30px] max-sm:text-[20px]">
+        Updates are going on 'Development' Branch
+      </h2>
+      <div className="flex">
+        <h3 className="mt-3">Do check it out 👉🏻</h3>
+        <Link
+          to={"https://github.com/amanjaiman1/Product_3D/tree/development"}
+          target={"_blank"}
+          className={"mt-2 ml-5 text-[20px] underline text-yellow-300"}
+        >
+          Click me{" "}
+        </Link>
+      </div>
       {/* <Header />
        <main className="min-[400px]:px-10 mt-8">
         <section className="container mx-auto">{children}</section>
