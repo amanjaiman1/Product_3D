2ebcedda36369ba99b464e5d2dcfe5a696e59dff
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-07-26 10:31:13 +0530

    feat: import and export design

--- src/components/CustomizerTopbar/index.jsx
+++ src/components/CustomizerTopbar/index.jsx
@@ -3,7 +3,9 @@ import Button from "../Button";
 import { useNavigate } from "react-router-dom";
 import Avatar from "./../Avatar";
 import { generateUUID } from "three/src/math/MathUtils";
-
+import { deserializeFromP3D } from "../../utils/utils";
+import { db } from "../../firebase/firebase";
+import { doc, setDoc } from "firebase/firestore";
 function CustomizerTopbar() {
   const [userInfo, setuserInfo] = useState({
     loading: true,
@@ -14,8 +16,27 @@ function CustomizerTopbar() {
     localStorage.clear();
     navigate("/login");
   };
-  const imageRedirect = () => {
-    navigate("/app/customizer/profile");
+  const handleImport = (event) => {
+    try {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        var deserializedData = await deserializeFromP3D(e.target.result);
+        delete deserializedData.id;
+        delete deserializedData.uid;
+
+        const { uid } = JSON.parse(await localStorage.getItem("userInfo"));
+        console.log(uid);
+        const docRef = doc(db, "designs", generateUUID());
+        await setDoc(docRef, {
+          uid: uid,
+          ...deserializedData,
+          lastUpdate: new Date().toUTCString(),
+        });
+        window.location.reload();
+      };
+      reader.readAsText(file);
+    } catch (error) {}
   };
   useEffect(() => {
     async function getUserInfo() {
@@ -42,7 +63,19 @@ function CustomizerTopbar() {
         >
           Design New T-shirt
         </Button>
-        <Button className={"bg-pink-600 hover:bg-pink-500"}>Import</Button>
+        <label
+          htmlFor="importfile"
+          className="bg-pink-500 hover:bg-pink-400 p-2 cursor-pointer text-white rounded-lg"
+        >
+          Import
+          <input
+            id="importfile"
+            onChange={handleImport}
+            hidden
+            type="file"
+            className={"bg-pink-600 hover:bg-pink-500 "}
+          />
+        </label>
         <Button className={"bg-violet-600 hover:bg-violet-500"} onClick={Logout}>
           Logout
         </Button>
