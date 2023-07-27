445afce5d474a77f99d62c0146024174a411d37a
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-07-27 10:39:33 +0530

    chore: routings done in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/app/Home";
 import ErrorPage from "./views/app/Error";
 import "./index.css";
-import Faq from "./views/app/Faq";
+
 import Login from "./views/auth/Login";
 import SignUp from "./views/auth/SignUp";
-import BlogPage from "./views/app/Blog";
+
 import { favicon } from "./assets";
 import AutoScroll from "./utils/AutoScroll";
 import ScrollToTop from "react-scroll-to-top";
 import MoonLoader from "react-spinners/MoonLoader";
-import ContributorPage from "./views/app/ContributorPage";
-import Guide from "./views/app/Guide";
+
 import Customizer from "./views/app/Customizer";
 import { HashLoader } from "react-spinners";
 import EditorHome from "./views/app/EditorHome";
@@ -21,6 +20,18 @@ import Profile from "./views/app/Profile";
 import PrivateRoute from "./utils/PrivateRoute";
 import Favourite from "./views/app/Favourite";
 import CreatePost from "./pages/Blog/CreatePost";
+
+// Pages Importing
+import ContributorPage from "./views/app/ContributorPage";
+import Guide from "./views/app/Guide";
+import BlogPage from "./views/app/Blog";
+import Faq from "./views/app/Faq";
+import CreatePage from "./views/app/Create";
+import ExplorePage from "./views/app/Explore";
+import HelpCenterPage from "./views/app/Helpcentre";
+import HowitWorksPage from "./views/app/How-it-works";
+import NewslettersPage from "./views/app/Newsletters";
+import SuggestionPage from "./views/app/Suggestions";
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -49,11 +60,19 @@ function App() {
           <Route Component={ErrorPage} path="/*" />
           <Route Component={Login} path="/login" />
           <Route Component={SignUp} path="/signup" />
+
+          <Route Component={ErrorPage} path="/*" />
+
+          {/* Pages Routing  */}
           <Route Component={BlogPage} path="/blog" />
           <Route Component={Guide} path="/guidebook" />
-          <Route Component={ErrorPage} path="/*" />
-          <Route Component={SignUp} path="/signup" />
-          <Route Component={Login} path="/login" />
+          <Route Component={CreatePage} path="/create" />
+          <Route Component={ExplorePage} path="/explore" />
+          <Route Component={HelpCenterPage} path="/helpcenter" />
+          <Route Component={HowitWorksPage} path="/how-it-works" />
+          <Route Component={NewslettersPage} path="/newsletters" />
+          <Route Component={SuggestionPage} path="/suggestions" />
+
           <Route
             element={
               <PrivateRoute>
