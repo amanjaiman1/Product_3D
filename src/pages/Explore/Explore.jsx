ff1f17e767cb6f001bdfcacacf9acbe219ffa176
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-07-27 10:36:37 +0530

    chore: pages render files added

---
+++ src/pages/Explore/Explore.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Explore = () => {
+  return <div>Explore</div>;
+};
+
+export default Explore;
---
+++ src/pages/Suggestions/Suggestions.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Suggestions = () => {
+  return <div>Suggestions</div>;
+};
+
+export default Suggestions;
