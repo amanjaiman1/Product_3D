2250ec2caa5b58d30acae90330a926f336454e38
PARAS <mehtaparas1901@gmail.com>
2023-07-28 18:38:44 +0530

    feat: navbar desgin

--- src/containers/Header/Navbar.jsx
+++ src/containers/Header/Navbar.jsx
@@ -15,15 +15,40 @@ const Navbar = () => {
     var navbar = document.querySelector("nav");
     navbar.classList.toggle("sticky", this.scrollY > 0);
   });
-
+  // text-[#D8BFD8] bg-gradient-to-r from-violet-600 to-pink-200
+  // shadow-lg bg-[#0f0f0fb4]
   return (
-    <nav className={`flex justify-around items-center p-4 shadow-lg bg-[#0f0f0fb4]`}>
-      <div>Fashion Forge</div>
+    <nav
+      className={`flex justify-center items-center bg-gradient-to-r from-violet-200 to-black  h-[90px] rounded-[35px] sticky top-3 max-w-[85%] mx-auto `}
+    >
+      <div>
+        <img width={"22%"} height={"10%"} src={newLogo} alt="Fashion Forge logo" />
+      </div>
       <ul className="flex space-x-10 justify-center items-center">
-        <li>Home</li>
-        <li>Contributors</li>
-        <li>GuideBook</li>
-        <li>Blog</li>
+        <li>
+          <a href="#" class="group text-white transition duration-100">
+            Home
+            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="group text-white transition duration-100">
+            Contributor
+            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="group text-white transition duration-100">
+            Guidebook
+            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="group text-white transition duration-100">
+            Blog
+            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black"></span>
+          </a>
+        </li>
         <li>
           <Button>Try Now</Button>
         </li>
