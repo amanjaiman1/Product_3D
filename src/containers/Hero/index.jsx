f44f44d974c43eb79b8844eb46072f26cad141d8
mahi-19 <upadhyayvivek.19@gmail.com>
2023-07-28 14:00:56 +0530

    feat: add the animated 3d model

--- src/containers/Hero/index.jsx
+++ src/containers/Hero/index.jsx
@@ -7,6 +7,9 @@ import { paraAnim } from "../../animation/motion";
 import { buttonAnim } from "../../animation/motion";
 import { imgAnim } from "../../animation/motion";
 import male from "./../../assets/image/male.png";
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./Model/Experience";
+
 function Hero() {
   const navigate = useNavigate();
 
@@ -23,13 +26,19 @@ function Hero() {
           Design Now
         </button>
       </div>
-      <div className="flex-1 flex-col justify-center h-screen object-contain">
-        <img
-          width={"100%"}
-          height={"100%"}
-          src={male}
-          className="object-contain max-h-full max-w-full"
-        />
+      <div className="flex-1 flex-col justify-center object-contain">
+        <Canvas
+          camera={{
+            position: [0, -8, 5],
+            fov: 30,
+            zoom: 1.3,
+            up: [0, 5, 4],
+            near: 0.8,
+          }}
+          style={{ height: "120vh", width: "100%", marginTop: "40%" }}
+        >
+          <Experience />
+        </Canvas>
       </div>
     </div>
   );
