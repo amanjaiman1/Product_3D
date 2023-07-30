827c36a2888739d7e041c6c69e93ed61148e2618
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-07-30 22:42:53 +0530

    feat: community container

---
+++ src/views/app/Community/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import EditorHomeLayout from "../../../layout/EditorHomeLayout";
+import CommunityContainer from "../../../containers/Community";
+
+function Community() {
+  return (
+    <EditorHomeLayout>
+      <CommunityContainer />
+    </EditorHomeLayout>
+  );
+}
+
+export default Community;
