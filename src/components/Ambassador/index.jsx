86c09e81ec18f956a58fb1cd2af5a548d01669f6
senthil-k8s <senthilathiban0@gmail.com>
2023-08-06 13:51:15 +0530

    feat(community): ambassador container

---
+++ src/components/Ambassador/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ambassador } from "../../assets";
+
+const Ambassador = () => {
+  return (
+    <div className="flex h-[50vh] ml-20 mr-20 p-5 rounded-lg">
+      <div className="w-1/2 h-full p-5">
+        <div className="flex text-3xl">
+          <span className="text-pink-600 font-bold"> | </span>
+          <h1 className="mr-3 ml-3 font-semibold">Ambassador</h1>
+          <span className="">Program</span>
+        </div>
+        <div className="description mt-5">
+          <p className="leading-relaxed">
+            Support the Fashion Froze core team through community and <br />
+            marketing initiatives while accumulating rewards
+          </p>
+        </div>
+        <button className="border p-3 bg-pink-500 rounded-full w-[25%] mt-10 font-normal hover:bg-violet-800">
+          Learn more >{" "}
+        </button>
+      </div>
+      <div className="w-1/2 h-full ml-20 ">
+        <img src={ambassador} alt="" className="w-[80%] h-[100%]" />
+      </div>
+    </div>
+  );
+};
+
+export default Ambassador;
--- src/views/app/CommunityLanding/index.jsx
+++ src/views/app/CommunityLanding/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HomeLayout from "../../../layout/HomeLayout.jsx";
 import CommunityLanding from "../../../pages/CommunityLanding/index.jsx";
+import Ambassador from "../../../components/ambassador/index.jsx";
 
 function Guide() {
   return (
     <HomeLayout>
       <CommunityLanding />
+      <Ambassador />
     </HomeLayout>
   );
 }
