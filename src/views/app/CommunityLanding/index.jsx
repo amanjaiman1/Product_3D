cf17ea478a03637ffce8f3d26f1619b1731639c4
senthil-k8s <senthilathiban0@gmail.com>
2023-08-06 18:29:47 +0530

    feat(community): designed community news cards

--- src/views/app/CommunityLanding/index.jsx
+++ src/views/app/CommunityLanding/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import HomeLayout from "../../../layout/HomeLayout.jsx";
 import CommunityLanding from "../../../pages/CommunityLanding/index.jsx";
 import Ambassador from "../../../components/Ambassador/index.jsx";
+import CommmunityNews from "../../../components/CommunityNews/index.jsx";
 
 function Guide() {
   return (
     <HomeLayout>
       <CommunityLanding />
       <Ambassador />
+      <CommmunityNews />
     </HomeLayout>
   );
 }
