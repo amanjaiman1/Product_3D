640b157fc673169fa31a756406d706021ba088e8
mahi-19 <upadhyayvivek.19@gmail.com>
2023-08-08 22:35:53 +0530

    feat: fixed navbar and add routes

--- src/containers/Header/Navbar.jsx
+++ src/containers/Header/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { newLogo } from "../../assets";
 import Button from "../../components/Button";
+import "../../index.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -19,20 +20,50 @@ const Navbar = () => {
     <nav className={`flex justify-between items-center h-[90px] mb-5 p-5 `}>
       {/* ... Your existing code ... */}
       <div className="flex justify-start ml-6">
-        <img src={newLogo} alt="Fashion Forge logo" className="w-24 h-24 mt-0.5" />
+        <Link to="/">
+          {" "}
+          {/* Set the Home route */}
+          <img src={newLogo} alt="Fashion Forge logo" className="w-24 h-24 mt-0.5" />
+        </Link>
       </div>
       <ul className="hidden md:flex lg:flex space-x-12 font-normal text-[17px] justify-center items-center mr-10">
         {["Home", "Contributor", "Guidebook", "Blog"].map((item, index) => (
           <li key={index}>
-            <a href="#" className="group text-[#d6e4ff] transition duration-100">
-              {item}
-              <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-violet-600"></span>
-            </a>
+            {item === "Home" && (
+              <Link to="/">
+                {" "}
+                {/* Set the Home route */}
+                <span className="group text-[#d6e4ff] transition duration-100">
+                  {item}
+                  <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-violet-600"></span>
+                </span>
+              </Link>
+            )}
+            {item === "Contributor" && (
+              <Link to="/contact">
+                {" "}
+                {/* Set the Contributor route */}
+                <span className="group text-[#d6e4ff] transition duration-100">
+                  {item}
+                  <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-violet-600"></span>
+                </span>
+              </Link>
+            )}
+            {item !== "Home" && item !== "Contributor" && (
+              <Link to={`/${item.toLowerCase()}`}>
+                {" "}
+                {/* Set other routes */}
+                <span className="group text-[#d6e4ff] transition duration-100">
+                  {item}
+                  <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-violet-600"></span>
+                </span>
+              </Link>
+            )}
           </li>
         ))}
         <li className="text-[#D8BFD8] lg:block">
           <Button className="rounded-[5px] w-[100%] hover:bg-violet-600 bg-transparent hover:transition-all md:w-auto px-4 py-2 text-center">
-            Try Now
+            <Link to="/try-now">Try Now</Link> {/* Set the Try Now route */}
           </Button>
         </li>
       </ul>
@@ -41,39 +72,66 @@ const Navbar = () => {
         type="button"
         className="inline-flex items-center justify-center md:hidden text-white   mr-5"
         onClick={() => setIsMobileMenuOpen((prev) => !prev)}
-      >
-        <svg
-          className={`w-6 h-6 ${isMobileMenuOpen ? "rotate-180" : ""}`}
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
+      ></button>
+      {/* Mobile View */}
+      <div className="ml-auto mr-5 mt-[-1rem] lg:hidden">
+        <button
+          className={`mobile-menu-toggle ${isMobileMenuOpen ? "open" : ""}`}
+          onClick={toggleMobileMenu}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-        </svg>
-      </button>
-
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </button>
+      </div>
       {isMobileMenuOpen && (
-        <div
-          className={`flex flex-col md:hidden align-items items-center bg-black rounded-xl absolute top-20 right-0 p-5 justify-center ${
-            isMobileMenuOpen ? "mobile-menu-open" : "mobile-menu-closed"
-          }`}
-        >
-          <ul className="font-normal text-[15px] justify-center items-center mr-10">
-            {["Home", "Contributor", "Guidebook", "Blog"].map((item, index) => (
-              <li key={index}>
-                <a href="#" className="group text-[#D8BFD8] transition duration-100">
-                  {item}
-                  <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-violet-600 mb-2"></span>
-                </a>
-              </li>
-            ))}
-          </ul>
+        <div className="mobile-menu" style={{ width: "70%", textAlign: "right" }}>
+          <Link
+            to="/"
+            className={`link ${location.pathname === "/" ? "active" : ""}`}
+            onClick={toggleMobileMenu}
+            style={{ justifyContent: "flex-end" }}
+          >
+            Home
+          </Link>
+          <Link
+            to="/guidebook"
+            className={`link ${location.pathname === "/guidebook" ? "active" : ""}`}
+            onClick={toggleMobileMenu}
+            style={{ justifyContent: "flex-end" }}
+          >
+            Guide Book
+          </Link>
+          <Link
+            to="/customize"
+            className={`link ${location.pathname === "/customize" ? "active" : ""}`}
+            onClick={toggleMobileMenu}
+            style={{ justifyContent: "flex-end" }}
+          >
+            Customize
+          </Link>
+          <Link
+            to="/contact"
+            className={`link ${location.pathname === "/contact" ? "active" : ""}`}
+            onClick={toggleMobileMenu}
+            style={{ justifyContent: "flex-end" }}
+          >
+            Contact Us
+          </Link>
+          <Link
+            to="/faq"
+            className={`link ${location.pathname === "/faq" ? "active" : ""}`}
+            onClick={toggleMobileMenu}
+            style={{ justifyContent: "flex-end" }}
+          >
+            FAQ's
+          </Link>
+          <button
+            className="justify-center border-[1px] p-2 rounded-md"
+            style={{ justifyContent: "center" }}
+          >
+            Try Free
+          </button>
         </div>
       )}
     </nav>
