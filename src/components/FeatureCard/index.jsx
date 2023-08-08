63b50e93d531a64f6ebd7f156658cd895bf28d39
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-08-08 23:06:05 +0530

    feat: enhanced the cards, not responsive one

--- src/components/FeatureCard/index.jsx
+++ src/components/FeatureCard/index.jsx
@@ -4,14 +4,14 @@ import "./style.css";
 function FeatureCard({ title, color, icon, desc }) {
   return (
     <div className="flex-1 w-full">
-      <div className="gap-y-10  m-5 p-6 flex flex-col justify-center align-items items-center box rounded-2xl">
+      <div className="gap-y-10  m-5 p-6 flex flex-col justify-center align-items items-center box">
         <div className="rounded-lg w-20 h-20 mx-4">
           <img src={icon} alt="" className="" />
         </div>
         <div className="font-mono text-3xl font-semibold text-center text-[#9400D3]">
           {title}
         </div>
-        <div className="text-secondary w-285 mx-auto px-4 leading-relaxed text-center text-[#D8BFD8]">
+        <div className="w-285 mx-auto px-4 leading-relaxed text-center text-[#D8BFD8]">
           {desc}
         </div>
       </div>
