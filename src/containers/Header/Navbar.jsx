083301b8974f12605d476510c89377a64ef8bc71
Harsh Patil <harshpatil2956@gmail.com>
2023-08-10 21:04:01 +0530

    explore routing added

--- src/containers/Header/Navbar.jsx
+++ src/containers/Header/Navbar.jsx
@@ -27,40 +27,42 @@ const Navbar = () => {
         </Link>
       </div>
       <ul className="hidden md:flex lg:flex space-x-12 font-normal text-[17px] justify-center items-center mr-10">
-        {["Home", "Contributor", "Guidebook", "Blog"].map((item, index) => (
-          <li key={index}>
-            {item === "Home" && (
-              <Link to="/">
-                {" "}
-                {/* Set the Home route */}
-                <span className="group text-[#d6e4ff] transition duration-100">
-                  {item}
-                  <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-violet-600"></span>
-                </span>
-              </Link>
-            )}
-            {item === "Contributor" && (
-              <Link to="/contact">
-                {" "}
-                {/* Set the Contributor route */}
-                <span className="group text-[#d6e4ff] transition duration-100">
-                  {item}
-                  <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-violet-600"></span>
-                </span>
-              </Link>
-            )}
-            {item !== "Home" && item !== "Contributor" && (
-              <Link to={`/${item.toLowerCase()}`}>
-                {" "}
-                {/* Set other routes */}
-                <span className="group text-[#d6e4ff] transition duration-100">
-                  {item}
-                  <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-violet-600"></span>
-                </span>
-              </Link>
-            )}
-          </li>
-        ))}
+        {["Home", "Contributor", "Guidebook", "Blog", "Explore"].map(
+          (item, index) => (
+            <li key={index}>
+              {item === "Home" && (
+                <Link to="/">
+                  {" "}
+                  {/* Set the Home route */}
+                  <span className="group text-[#d6e4ff] transition duration-100">
+                    {item}
+                    <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-violet-600"></span>
+                  </span>
+                </Link>
+              )}
+              {item === "Contributor" && (
+                <Link to="/contact">
+                  {" "}
+                  {/* Set the Contributor route */}
+                  <span className="group text-[#d6e4ff] transition duration-100">
+                    {item}
+                    <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-violet-600"></span>
+                  </span>
+                </Link>
+              )}
+              {item !== "Home" && item !== "Contributor" && (
+                <Link to={`/${item.toLowerCase()}`}>
+                  {" "}
+                  {/* Set other routes */}
+                  <span className="group text-[#d6e4ff] transition duration-100">
+                    {item}
+                    <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-violet-600"></span>
+                  </span>
+                </Link>
+              )}
+            </li>
+          )
+        )}
         <li className="text-[#D8BFD8] lg:block">
           <Button className="rounded-[5px] w-[100%] hover:bg-violet-600 bg-transparent hover:transition-all md:w-auto px-4 py-2 text-center">
             <Link to="/app/customizer">Try Now</Link> {/* Set the Try Now route */}
@@ -117,6 +119,14 @@ const Navbar = () => {
             style={{ justifyContent: "flex-end" }}
           >
             Contact Us
+          </Link>
+          <Link
+            to="/explore"
+            className={`link ${location.pathname === "/explore" ? "active" : ""}`}
+            onClick={toggleMobileMenu}
+            style={{ justifyContent: "flex-end" }}
+          >
+            Explore
           </Link>
           <Link
             to="/faq"
